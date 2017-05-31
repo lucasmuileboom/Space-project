deb0af46350d4ab1c7bc017981226dc9dd2c7efd
blizzard190 <22256@ma-web.nl>
2017-05-31 14:28:18 +0200

    Enemy
    I'm awsome dog!!

---
+++ Assets/Enemy/Enemy_idle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_idle : IEnemyState
+{
+    private Enemy enemy;
+
+    private float idleTimer;
+
+    private float idleDuration = 5;
+
+    public void Enter(Enemy enemy)
+    {
+        this.enemy = enemy;
+    }
+
+    public void Excute()
+    {
+        Idle();
+
+        if (enemy.Target != null && enemy.InMeleeRange)
+        {
+            enemy.ChangeState(new Enemy_mellee());
+        }
+        else if (enemy.Target != null)
+        {
+            enemy.ChangeState(new Enemy_patrol());
+        }
+    }
+
+    public void Exit()
+    {
+      
+    }
+
+    public void OnTriggerEnter(Collider2D other)
+    {
+
+    }
+
+    private void Idle()
+    {
+        enemy.MyAnimator.SetFloat("speed", 0);
+
+        idleTimer += Time.deltaTime;
+
+        if(idleTimer >= idleDuration)
+        {
+            enemy.ChangeState(new Enemy_patrol());
+        }
+    }
+}
---
+++ Assets/Enemy/Enemy_mellee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_mellee : IEnemyState
+{
+    private Enemy enemy;
+
+    private float attackTimer;
+    private float attackCoolDown = 3;
+    private bool canAttack = true;
+
+    public void Enter(Enemy enemy)
+    {
+        this.enemy = enemy;
+    }
+
+    public void Excute()
+    {
+       Attack();
+      //if (enemy.Target != null)
+      //  {
+      //      enemy.Move();
+      //  }
+        if(enemy.Target != null)
+        {
+            enemy.ChangeState(new Enemy_idle());
+        }
+    }
+
+    public void Exit()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider2D other)
+    {
+
+    }
+
+    private void Attack()
+    {
+        attackTimer += Time.deltaTime;
+
+        if(attackTimer >= attackCoolDown)
+        {
+            canAttack = true;
+            attackTimer = 0;
+        }
+
+        if (canAttack)
+        {
+            canAttack = false;
+            enemy.MyAnimator.SetTrigger("attack");
+        }
+    }
+}
